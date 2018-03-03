73b4e92137843a5cd7e8c4b1851f1aeee0035c53
Patrick Perry <patperry@gmail.com>
2018-03-03 11:25:19 -0500

    start 'schema'

---
+++ R/schema.R
@@ -0,0 +1,50 @@
+#  Copyright 2018 Patrick O. Perry.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+schema <- function(x)
+{
+    UseMethod("schema")
+}
+
+
+schema.default <- function(x)
+{
+    if (is.null(x))
+        return(NULL)
+
+    d <- dim(x)
+    r <- length(d)
+    if (r <= 1) {
+        x[0]
+    } else if (r == 2) {
+        x[0, , drop = TRUE]
+    } else {
+        stop(sprintf("cannot get schema of rank-%.0f object", r))
+    }
+}
+
+
+schema.record <- function(x)
+{
+    n <- length(x)
+    y <- vector("list", n)
+    for (i in seq_len(n)) {
+        elt <- x[[i]]
+        if (is.null(elt))
+            next
+        y[[i]] <- schema(elt)
+    }
+    names(y) <- names(x)
+    as.record(y)
+}
