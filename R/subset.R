bf61a59fa2f661e5923da2be7e5aa583dd987d79
Patrick Perry <patperry@gmail.com>
2018-03-31 15:31:43 -0400

    add 'subset.dataset'

---
+++ R/subset.R
@@ -0,0 +1,34 @@
+
+subset.dataset <- function(x, ...)
+{
+    x <- as.dataset(x)
+    exprs <- substitute(list(...))
+    for (i in seq_along(exprs)) {
+        exprs <- eval.parent(call("scope", x, exprs))
+    }
+
+    n <- length(exprs)
+    if (n == 0)
+        return(x)
+
+    nrow <- nrow(x)
+
+    for (i in seq_len(n)) {
+        e <- exprs[[i]]
+        e <- as.logical(e)
+
+        ne <- length(e)
+        if (nrow != ne) {
+            fmt <- "mismatch: data has %.0f rows, condition length is %.0f"
+            stop(sprintf(fmt, nrow, ne))
+        }
+
+        if (i == 1) {
+            mask <- e
+        } else {
+            mask <- mask & e
+        }
+    }
+
+    x[mask, ]
+}
