038342b45652a400da8aacb5a6a726deb7127c02
Patrick Perry <patperry@gmail.com>
2018-04-05 17:06:43 -0400

    document 'scope'

--- R/scope.R
+++ R/scope.R
@@ -1,41 +1,43 @@
 
-scope <- function(x, expr, env = NULL)
+scope <- function(x, expr, envir = NULL)
 {
     UseMethod("scope")
 }
 
 
-scope.default <- function(x, expr, env = NULL)
+scope.default <- function(x, expr, envir = NULL)
 {
-    x    <- if (is.record(x) || is.data.frame(x)) x else as.list(x)
-    expr <- substitute(expr)
-    env  <- if (is.null(env)) parent.frame() else as.environment(env)
+    x     <- if (is.record(x) || is.data.frame(x)) x else as.list(x)
+    expr  <- substitute(expr)
+    envir <- if (is.null(envir)) parent.frame() else as.environment(envir)
 
-    expr <- eval_I(expr, env)
-    eval(expr, x, env)
+    expr <- eval_I(expr, envir)
+    eval(expr, x, envir)
 }
 
 
-eval_I <- function(expr, env)
+eval_I <- function(expr, envir)
 {
-    if (is.call(expr)) {
-        if (expr[[1]] == as.name("I")) {
-            narg <- length(expr)
-            if (narg == 2) {
-                return(eval(expr[[2]], NULL, env))
-            } else if (narg == 1) {
-                stop("empty I() call")
-            } else {
-                stop(sprintf("too many arguments (%.0f) to I()", narg - 1))
-            }
+    if (!is.call(expr))
+        return(expr)
+
+    if (expr[[1]] == as.name("I")) {
+        narg <- length(expr)
+        if (narg == 2) {
+            return(eval(expr[[2]], NULL, envir))
+        } else if (narg == 1) {
+            stop("empty I() call")
         } else {
-            for (i in seq_along(expr)) {
-                e <- expr[[i]]
-                if (!is.call(e))
-                    next
-                expr[[i]] <- eval_I(e, env)
-            }
+            stop(sprintf("too many arguments (%.0f) to I()", narg - 1))
+        }
+    } else {
+        for (i in seq_along(expr)) {
+            e <- expr[[i]]
+            if (!is.call(e))
+                next
+            expr[[i]] <- eval_I(e, envir)
         }
     }
+
     expr
 }
