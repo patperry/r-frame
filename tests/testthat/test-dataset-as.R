8c4357c1946f41ae8baaaa4f58d097e52938bc2d
Patrick Perry <patperry@gmail.com>
2018-04-06 14:45:48 -0400

    add dataset conversion tests

--- tests/testthat/test-dataset-as.R
+++ tests/testthat/test-dataset-as.R
@@ -26,3 +26,22 @@ test_that("as.data.frame, optional", {
     expect_equal(names(as.data.frame(x, optional = FALSE)), "X.")
     expect_equal(names(as.data.frame(x, optional = TRUE)), "#")
 })
+
+
+test_that("as.matrix", {
+    x <- as.dataset(mtcars)
+    x <- as.matrix(x)
+
+    y <- as.matrix(mtcars)
+    rownames(y) <- NULL
+    storage.mode(y) <- "list"
+    expect_equal(x, y)
+})
+
+
+test_that("as.matrix with matrix column", {
+    x <- dataset(mat = matrix(1:6, 3, 2))
+    y <- matrix(list(c(1L, 4L), c(2L, 5L), c(3L, 6L)), 3, 1)
+    colnames(y) <- "mat"
+    expect_equal(as.matrix(x), y)
+})
